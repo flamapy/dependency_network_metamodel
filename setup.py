16d22cd2f62a632a5eda763bfe5fa46be5deeedd
GermanMT <germanoctako@gmail.com>
2022-10-24 14:34:26 +0200

    feat: Add flamapy as core in imports

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/GermanMT/dependency_network_metamodel',
+    packages = setuptools.find_namespace_packages(include=['flamapy.*']),
     classifiers = [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
