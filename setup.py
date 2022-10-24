d702cc20e0b17babaa13da8cb21591691f2e4e30
GermanMT <germanoctako@gmail.com>
2022-10-24 17:30:40 +0200

    chore: Reorganise files

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = 'dn_metamodel',
-    version = '1.1.3',
+    name = 'flamapy-dn',
+    version = '1.2.0',
     author = 'Antonio Germán Márquez Trujillo',
     author_email = 'amtrujillo@us.es',
     description = 'This repo host the dependency network model concrete classes',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/GermanMT/dependency_network_metamodel',
-    packages = setuptools.find_namespace_packages(include=['flamapy.*']),
+    packages = setuptools.find_namespace_packages(include = ['famapy.*']),
     classifiers = [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
