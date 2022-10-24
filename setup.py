076311a6e1bb71ea47bc535ceb96bff324b268c3
GermanMT <germanoctako@gmail.com>
2022-10-24 15:37:45 +0200

    fix: Rename namespace

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name = 'dn_metamodel',
-    version = '1.1.0',
+    version = '1.1.3',
     author = 'Antonio Germán Márquez Trujillo',
     author_email = 'amtrujillo@us.es',
     description = 'This repo host the dependency network model concrete classes',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/GermanMT/dependency_network_metamodel',
-    packages = ['dn_metamodel'],
+    packages = setuptools.find_namespace_packages(include=['flamapy.*']),
     classifiers = [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
