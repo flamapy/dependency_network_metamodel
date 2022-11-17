9c037dde99b76fb617af0c6d8fe32697c9071fae
GermanMT <germanoctako@gmail.com>
2022-11-17 19:45:40 +0100

    feat: Refactor info operation

--- flamapy/metamodels/dn_metamodel/operations/network_info.py
+++ flamapy/metamodels/dn_metamodel/operations/network_info.py
@@ -1,6 +1,6 @@
 from flamapy.core.operations import Operation
 
-from flamapy.metamodels.dn_metamodel.models import DependencyNetwork, Version
+from flamapy.metamodels.dn_metamodel.models import DependencyNetwork, RequirementFile, Version
 
 
 class NetworkInfo(Operation):
@@ -23,26 +23,39 @@ class NetworkInfo(Operation):
 
     def execute(self, model: DependencyNetwork) -> None:
         for requirement_file in model.requirement_files:
-            self.result['constraints'] += len(requirement_file.packages)
-            for package in requirement_file.packages:
-                if package.name not in self.direct_dependencies:
-                    self.direct_dependencies.append(package.name)
-                for version in package.versions:
-                    for cve in version.cves:
-                        if cve['id'] not in self.direct_cves: self.direct_cves.append(cve['id'])
-                    self.indirect(version)
+            self.search(requirement_file, 'direct')
         self.result['direct_dependencies'] = len(self.direct_dependencies)
         self.result['indirect_dependencies'] = len(self.indirect_dependencies)
         self.result['direct_cves'] = len(self.direct_cves)
         self.result['indirect_cves'] = len(self.indirect_cves)
 
-    def indirect(self, parent_version: Version) -> None:
-        if hasattr(parent_version, 'packages'):
-            self.result['constraints'] += len(parent_version.packages)
-            for package in parent_version.packages:
-                if package.name not in self.indirect_dependencies:
-                    self.indirect_dependencies.append(package.name)
-                for version in package.versions:
-                    for cve in version.cves:
-                        if cve['id'] not in self.indirect_cves: self.indirect_cves.append(cve['id'])
-                    self.indirect(version)
+    def search(self, parent: Version | RequirementFile, level: str) -> None:
+        self.result['constraints'] += len(parent.packages)
+        for package in parent.packages:
+            if (
+                package.name not in self.indirect_dependencies and
+                package.name not in self.direct_dependencies
+            ):
+                self.add_dependencie(package.name, level)
+            for version in package.versions:
+                for cve in version.cves:
+                    if (
+                        cve['id'] not in self.indirect_cves and 
+                        cve['id'] not in self.direct_cves
+                    ):
+                        self.add_cve(cve['id'], level)
+                self.search(version, 'indirect')
+
+    def add_dependencie(self, dependencie_name: str, level: str) -> None:
+        match level:
+            case 'direct':
+                self.direct_dependencies.append(dependencie_name)
+            case 'indirect':
+                self.indirect_dependencies.append(dependencie_name)
+
+    def add_cve(self, cve_id: str, level: str) -> None:
+        match level:
+            case 'direct':
+                self.direct_cves.append(cve_id)
+            case 'indirect':
+                self.indirect_cves.append(cve_id)
